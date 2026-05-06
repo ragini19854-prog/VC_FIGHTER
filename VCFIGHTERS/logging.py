ec304b2b55d1e2b330150ead6f52ec9fff43369f
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-05-06 22:22:38 +0530

    Update logging.py

--- VCFIGHTERS/logging.py
+++ VCFIGHTERS/logging.py
@@ -1,1 +1,21 @@
+import logging
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        logging.FileHandler("VCFighter.log"),
+        logging.StreamHandler(),
+    ],
+)
+
+# Noise suppress karo inke logs ka
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logging.getLogger("asyncio").setLevel(logging.ERROR)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
